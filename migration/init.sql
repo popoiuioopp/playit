5bfeab4cf564c1738a49a893b001602b438c555f
bigge <pongpich.sing@gmail.com>
2024-11-24 10:06:00 +0700

    update timestamptz

--- migration/init.sql
+++ migration/init.sql
@@ -3,7 +3,9 @@ CREATE TABLE
         "id" UUID NOT NULL UNIQUE DEFAULT gen_random_uuid (),
         "username" TEXT NOT NULL UNIQUE,
         "youtube_channel" TEXT,
-        "twitch_channel" TEXT
+        "twitch_channel" TEXT,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE INDEX "performer_index_0" ON "performer" ("id", "username");
@@ -12,7 +14,9 @@ CREATE TABLE
     "performance" (
         "id" UUID NOT NULL UNIQUE DEFAULT gen_random_uuid (),
         "performer_id" UUID NOT NULL,
-        "status" TEXT NOT NULL
+        "status" TEXT NOT NULL,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE INDEX "performance_index_0" ON "performance" ("id", "performer_id");
@@ -25,7 +29,9 @@ CREATE TABLE
         "artist" TEXT,
         "status" TEXT NOT NULL,
         "requester" TEXT NOT NULL,
-        "requester_platform" TEXT NOT NULL
+        "requester_platform" TEXT NOT NULL,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE INDEX "request_index_0" ON "request" ("id", "performance_id");
